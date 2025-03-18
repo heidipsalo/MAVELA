b9b0711bb2df6a7104684e95ca108d3413115eed
Salo Heidi <heidi.salo@aalto.fi>
2025-03-18 10:47:19 +0200

    added possibility to change a parameter value from loading parameters from the report-file.

--- scripts_to_generate_input_files/settings.py
+++ scripts_to_generate_input_files/settings.py
@@ -10,7 +10,7 @@ def init():
     global flush_bat_str, dir_flush, input_file_path_plots, plots_folder, location, year, coordinates, veg_season, drain_spacing, slope, temp_folder, input_folder, drain_type, control_level, control_time
     global top_layer_soil, macros_top_soil, bottom_layer_soil, macros_bottom_soil, user_depth, user_soil, open_ditch, open_ditch_dist
 
-    dir_flush = r"C:\Users\hpsalo\src\Flush_03_CD\Flush_03_CD\Release"
+    dir_flush = r"C:\Users\hpsalo\Mavela\flush_03_cd\Release"
     temp_folder, input_folder, plots_folder, output_dir = create_folders(dir_flush)
 
     input_file_path_plots = fr"{output_dir}\log.txt"
@@ -24,7 +24,53 @@ def init():
 
     use_file = input("Load user inputs from the file? (Y/N):\n").capitalize()
     if use_file == 'Y':
-      open_ditch_dist, open_ditch, location, year, coordinates, veg_season, drain_spacing, slope, drain_type, control_level, control_time, user_depth, user_soil, top_layer_soil, macros_top_soil, bottom_layer_soil, macros_bottom_soil = download_input_from_file(input_file_path)
+      open_ditch_dist, open_ditch, location, year, coordinates, veg_season, drain_spacing, slope, drain_type, control_level, control_time, user_depth, user_soil, top_layer_soil, macros_top_soil, bottom_layer_soil, macros_bottom_soil, df_user_inputs = download_input_from_file(input_file_path)
+      
+      # Print information for the user about the current parameters.
+      print("Current parameters are: ")
+      print(df_user_inputs)
+      
+      # Ask user which parameter will be changed.
+      change_param = input("What parameter you want to change? (0 - Drain spacing, 1- Drain depth, 2 - Drain type, 3 - Ditch, 4 - Soil type): ")
+      if change_param == '0':
+          drain_spacing = float(input("Drain spacing in [m]:\n"))
+          df_user_inputs["drain_spacing"] = drain_spacing
+      elif change_param == '1':
+          user_depth = float(input("What is the depth of the subsurface drainage? (0.02m - 1.9m available) \n"))
+          df_user_inputs["user_depth"] = user_depth
+      elif change_param == '2':
+          drain_type = input("Do you want to model control or conventional drainage? (Type control or conventional)\n").capitalize()
+          df_user_inputs["drain_type"] = drain_type
+
+          if drain_type == "Control":
+            control_level = float(input("What is the control level in the control wells (m)? (0 - surface level, 2.4 - deepest soil layer)\n"))
+            if control_level > user_depth:
+              pass  # print warning and try again
+            df_user_inputs["control_level"] = control_level
+            control_time = input("What is drainage control time in month-day format via space? Press Enter when finished.\n")  # list of min one to max three values; time range within the year
+            df_user_inputs["control_time"] = control_time
+      elif change_param == '3':
+          open_ditch = input("Do you have open ditch? (Y/N):\n ").capitalize()
+          df_user_inputs["open_ditch"] = open_ditch
+          if open_ditch == "Y":
+            open_ditch_dist = float(input("What is the distance to open ditch (m)?\n"))
+            df_user_inputs["open_ditch_dist"] = open_ditch_dist
+      elif change_param == '4':
+          user_soil = input("Do you want to use available soil libraries? (Y/N):\n ").capitalize()
+          df_user_inputs["user_soil"] = user_soil
+          if user_soil == "Y":
+            top_layer_soil = input("What is your top layer soil type (clay, silt and peat soils are available at the moment)?\n").capitalize()
+            df_user_inputs["top_layer_soil"] = top_layer_soil
+
+            macros_top_soil = input("What is your top layer soil macropores size (sizes available high and low)?\n").capitalize()
+            df_user_inputs["macros_top_soil"] = macros_top_soil
+
+            bottom_layer_soil = input("What is your bottom layer soil type (clay, silt and peat soils are available at the moment)?\n").capitalize()
+            df_user_inputs["bottom_layer_soil"] = bottom_layer_soil
+
+            macros_bottom_soil = input("What is your bottom layer soil macropores size (sizes available high and low)?\n").capitalize()
+            df_user_inputs["macros_bottom_soil"] = macros_bottom_soil
+
 
     else:
       user_inputs = {}
@@ -86,15 +132,18 @@ def init():
         macros_bottom_soil = input("What is your bottom layer soil macropores size (sizes available high and low)?\n").capitalize()
         user_inputs["macros_bottom_soil"] = macros_bottom_soil
 
-      df = pd.DataFrame(data=user_inputs, index=[0])
-      df.to_csv(fix_path(input_folder) + "report.txt")
+      df_user_inputs = pd.DataFrame(data=user_inputs, index=[0])
+    
+    # User inputs will be saved to a report-file. 
+    # Also modified parameters will be saved, if parameters are loaded from file.
+    df_user_inputs.to_csv(fix_path(input_folder) + "report.txt")
 
     if user_soil == "N":
       import soil_param_user
 
 
 def download_input_from_file(input_file_path):
-  df = pd.read_csv(input_file_path)
+  df = pd.read_csv(input_file_path, index_col=0)
 
   year = df['year'].values[0]
   location = df['location'].values[0]
@@ -135,7 +184,7 @@ def download_input_from_file(input_file_path):
 
   return (open_ditch_dist, open_ditch, location, year, coordinates, veg_season, drain_spacing, slope, drain_type,
     control_level, control_time, user_depth, user_soil, top_layer_soil,
-    macros_top_soil, bottom_layer_soil, macros_bottom_soil)
+    macros_top_soil, bottom_layer_soil, macros_bottom_soil, df)
 
 
 def create_folders(flush_dir):
